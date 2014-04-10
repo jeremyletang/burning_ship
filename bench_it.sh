3eabc4e29a8fab5ebdd7ce848beb9b4c6a3c4966
JeremyLetang <letang.jeremy@gmail.com>
2014-04-09 21:23:56 -0400

    add more test

---
+++ bench_it.sh
@@ -0,0 +1,108 @@
+cmake .
+./clean.sh
+make
+echo "testing for 1 core..."
+echo "1 core" >> results.txt
+echo "test 1."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 1 -s 25600 14400 -i 500 >> results.txt
+echo "test 2."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 1 -s 25600 14400 -i 500 >> results.txt
+echo "test 3."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 1 -s 25600 14400 -i 500 >> results.txt
+echo "test 4."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 1 -s 25600 14400 -i 500 >> results.txt
+echo "test 5."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 1 -s 25600 14400 -i 500 >> results.txt
+
+echo "testing for 2 core..."
+echo "2 core" >> results.txt
+echo "test 1."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 2 -s 25600 14400 -i 500 >> results.txt
+echo "test 2."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 2 -s 25600 14400 -i 500 >> results.txt
+echo "test 3."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 2 -s 25600 14400 -i 500 >> results.txt
+echo "test 4."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 2 -s 25600 14400 -i 500 >> results.txt
+echo "test 5."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 2 -s 25600 14400 -i 500 >> results.txt
+
+echo "testing for 3 core..."
+echo "3 core" >> results.txt
+echo "test 1."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 3 -s 25600 14400 -i 500 >> results.txt
+echo "test 2."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 3 -s 25600 14400 -i 500 >> results.txt
+echo "test 3."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 3 -s 25600 14400 -i 500 >> results.txt
+echo "test 4."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 3 -s 25600 14400 -i 500 >> results.txt
+echo "test 5."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 3 -s 25600 14400 -i 500 >> results.txt
+
+echo "testing for 4 core..."
+echo "4 core" >> results.txt
+echo "test 1."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 4 -s 25600 14400 -i 500 >> results.txt
+echo "test 2."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 4 -s 25600 14400 -i 500 >> results.txt
+echo "test 3."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 4 -s 25600 14400 -i 500 >> results.txt
+echo "test 4."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 4 -s 25600 14400 -i 500 >> results.txt
+echo "test 5."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 4 -s 25600 14400 -i 500 >> results.txt
+
+echo "testing for 5 core..."
+echo "5 core" >> results.txt
+echo "test 1."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 5 -s 25600 14400 -i 500 >> results.txt
+echo "test 2."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 5 -s 25600 14400 -i 500 >> results.txt
+echo "test 3."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 5 -s 25600 14400 -i 500 >> results.txt
+echo "test 4."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 5 -s 25600 14400 -i 500 >> results.txt
+echo "test 5."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 5 -s 25600 14400 -i 500 >> results.txt
+
+echo "testing for 6 core..."
+echo "6 core" >> results.txt
+echo "test 1."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 6 -s 25600 14400 -i 500 >> results.txt
+echo "test 2."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 6 -s 25600 14400 -i 500 >> results.txt
+echo "test 3."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 6 -s 25600 14400 -i 500 >> results.txt
+echo "test 4."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 6 -s 25600 14400 -i 500 >> results.txt
+echo "test 5."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 6 -s 25600 14400 -i 500 >> results.txt
+
+echo "testing for 7 core..."
+echo "7 core" >> results.txt
+echo "test 1."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 7 -s 25600 14400 -i 500 >> results.txt
+echo "test 2."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 7 -s 25600 14400 -i 500 >> results.txt
+echo "test 3."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 7 -s 25600 14400 -i 500 >> results.txt
+echo "test 4."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 7 -s 25600 14400 -i 500 >> results.txt
+echo "test 5."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 7 -s 25600 14400 -i 500 >> results.txt
+
+echo "testing for 8 core..."
+echo "8 core" >> results.txt
+echo "test 1."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 8 -s 25600 14400 -i 500 >> results.txt
+echo "test 2."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 8 -s 25600 14400 -i 500 >> results.txt
+echo "test 3."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 8 -s 25600 14400 -i 500 >> results.txt
+echo "test 4."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 8 -s 25600 14400 -i 500 >> results.txt
+echo "test 5."
+./burning_ship -d 8 6 -c 1.6 0.005 -core 8 -s 25600 14400 -i 500 >> results.txt
+
+# ./burning_ship -d 8 6 -c 1.6 0.005 -core 1 -s 25600 14400 >> results.txt
